e8e4b27758018b4de86b7f43ea8d4d503feaf943
tilogie <tilogie@student.s19.be>
2026-01-09 21:49:07 +0100

    fix: adjust email trimming length in header generation script

--- src/header42.sh
+++ src/header42.sh
@@ -83,7 +83,7 @@ generate_42_header() {
 	
 	# Trim functions
 	local trimlogin="${user:0:9}"
-	local trimemail="${mail:0:$((contentlen - 13))}"
+	local trimemail="${mail:0:$((contentlen - 16))}"
 	
 	# Generate bigline
 	local fillcount=$((linelen - 2 - ${#start} - ${#end}))
